fa099fe38cd92cb9441840db4aa80b33ec76eae9
Oleg14U <hohleghek@yandex.ru>
2023-06-22 16:20:28 +0300

    Stage 4

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,13 +1,13 @@
-from gendiff.formaters.stylish import render_stylish
-from gendiff.formaters.plain import render_plain
-from gendiff.formaters.json import render_json
+from gendiff.formatters.stylish import render_stylish
+from gendiff.formatters.plain import render_plain
+from gendiff.formatters.json import render_json
 
 
-def get_formatter(formater):
-    if formater == 'stylish':
+def get_formatter(formatter):
+    if formatter == 'stylish':
         return render_stylish
-    if formater == 'plain':
+    if formatter == 'plain':
         return render_plain
-    if formater == 'json':
+    if formatter == 'json':
         return render_json
-    raise ValueError(f"Unrecognized formater: {formater}")
+    raise ValueError(f"Unrecognized formatter: {formatter}")
